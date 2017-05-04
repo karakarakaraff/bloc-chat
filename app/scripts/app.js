6152e7c714c723167899fd84ff0a503df282b4b5
Kara <kara.a.flaherty@gmail.com>
2017-05-03 18:36:47 -0600

    Added the ability to create a chat room
    * Added the ability to create a chat room
    
    * Made requested changes to ModalCtrl.js
    
    * Removed commented out code/notes from Room.js

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -15,6 +15,6 @@
   }
 
   angular
-    .module('blocChat', ['ui.router', 'firebase'])
+    .module('blocChat', ['ui.bootstrap', 'ui.router', 'firebase'])
     .config(config);
 })();
--- app/scripts/services/Room.js
+++ app/scripts/services/Room.js
@@ -1,10 +1,29 @@
 (function() {
   function Room($firebaseArray) {
+    var Room = {};
+
+    /**
+     * @desc References the Firebase database and queries the "rooms" data
+     * @type {Object}
+     */
     var ref = firebase.database().ref().child("rooms");
+
+    /**
+     * @desc Creates an array of the "rooms" data queried above
+     * @type {Array}
+     */
     var rooms = $firebaseArray(ref);
 
+    /**
+     * @function Room.add (included in return statement)
+     * @desc Creates a new chat room by adding it to the database's "rooms" data
+     * @param {Object} newRoom
+     */
     return {
-      all: rooms
+      all: rooms,
+      add: function(newRoom) {
+        rooms.$add(newRoom);
+      }
     };
   }
 
